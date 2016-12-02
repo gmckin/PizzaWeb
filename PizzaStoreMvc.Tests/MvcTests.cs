1bd6fccec6579613473c5139034bf2d652dabe24
greg mckinney <gmck95@msn.com>
2016-12-01 21:00:42 -0500

    xUnit [#135393655]

---
+++ PizzaStoreMvc.Tests/MvcTests.cs
@@ -0,0 +1,48 @@
+﻿using PizzaStoreMvc.Client.DomainModels;
+using PizzaStoreMvc.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PizzaStoreMvc.Tests
+{
+  public class MvcTests
+  {
+    private PizzaStoreAPIContext db = new PizzaStoreAPIContext();
+
+    [Fact]
+    public void Test_GetCustomerID()
+    {
+      var c = new Customer();
+      
+      var actual = c.CustomerId.ToString();
+
+      Assert.NotNull(actual);
+    }
+
+
+    [Fact]
+    public void Test_GetCustomerAddressID()
+    {
+      var c = new Customer();
+
+      var actual = c.AddressID.ToString();
+
+      Assert.NotNull(actual);
+    }
+
+    [Fact]
+    public void Test_GetCustomerCityID()
+    {
+      var c = new Customer();
+
+      var actual = c.CityID.ToString();
+
+      Assert.NotNull(actual);
+    }
+
+  }
+}
